4719f207b0040ae9b06796a0d406e99d0d2bb747
cooldude72463 <kieransmollet0@gmail.com>
2018-04-21 19:51:38 +0100

    test

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ app.get('/SearchPage', function(req, res) {
  res.render('pages/SearchPage');
 });
 
+
+app.use(express.static("CSS/MainPage.css" + '/public'));
+
 app.listen(8080);
 console.log('8080 is the magic port');
